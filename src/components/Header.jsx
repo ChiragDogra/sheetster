efd42f1bf50bb2f6a9c82ed09aee9ed78da249f1
ChiragDogra <chirag.dogra79@gmail.com>
2023-05-03 17:39:46 +0530

    added icon and title to page

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "@arco-design/web-react/dist/css/arco.css";
+
+function Header() {
+  return (
+    <div className=''>
+        <div className=''>
+          <h1>Icon</h1>
+        </div>
+
+        <div className=''>
+
+        </div>
+
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
