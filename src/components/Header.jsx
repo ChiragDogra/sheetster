41070b5069a39c349d65f54c4af81b5d6cde4b29
ChiragDogra <chirag.dogra79@gmail.com>
2023-05-03 19:04:09 +0530

    made draweer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import "@arco-design/web-react/dist/css/arco.css";
 import { IconMenu, IconSearch } from "@arco-design/web-react/icon";
 import { IconButton, Avatar } from "@mui/material";
+import TemporaryDrawer from "./TemporaryDrawer";
 
 function Header() {
   return (
     <div className="sticky px-3 py-1 flex justify-between bg-gray-100 drop-shadow-sm">
       <div className="flex items-center">
-        <IconButton>
+        {/* <IconButton>
           <IconMenu className="" />
-        </IconButton>
+        </IconButton> */}
+
+        <TemporaryDrawer/>
       </div>
 
       <div className="flex items-center">
         <input
           type="text"
-          className=" px-2  border-2 rounded-lg "
+          className=" px-2  border-2 rounded-lg h-8"
           name="search"
         />
         <IconButton>
